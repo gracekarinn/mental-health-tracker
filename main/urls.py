8a6f090ae1406ecff5b77411577efab66e84058d
gracekarinn <gracekarina12@gmail.com>
2024-10-02 15:44:57 +0700

    refactor: Update delete-mood URL in main/urls.py

--- main/urls.py
+++ main/urls.py
@@ -14,6 +14,6 @@ urlpatterns = [
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
     path('edit-mood/<uuid:id>', edit_mood, name='edit_mood'),
-    path('delete-mood/<uuid:id>', delete_mood, name='delete_mood'),
+    path('delete/<uuid:id>', delete_mood, name='delete_mood'), 
     path('create-mood-entry-ajax', add_mood_entry_ajax, name='add_mood_entry_ajax'),
 ]
