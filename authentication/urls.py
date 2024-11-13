1fd48574ad26514d4a294f3e3133cf2101f7b88e
gracekarinn <gracekarina12@gmail.com>
2024-11-13 13:05:03 +0700

    feat: add logout URL to authentication routes

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from authentication.views import login, register, create_mood_flutter
+from authentication.views import login, register, create_mood_flutter, logout
 
 app_name = 'authentication'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/', login, name='login'),
     path('register/', register, name='register'),
     path('create-flutter/', create_mood_flutter, name='create_mood_flutter'),
+    path('logout/', logout, name='logout'),
 ]
