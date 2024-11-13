d08e0013530b220a95cc9ebe4bdecffc45c4bdba
gracekarinn <gracekarina12@gmail.com>
2024-11-13 13:35:50 +0700

    feat: remove create mood Flutter view from authentication views

--- authentication/views.py
+++ authentication/views.py
@@ -2,7 +2,6 @@ from django.contrib.auth import authenticate, login as auth_login, logout as aut
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
-from main.models import MoodEntry
 import json
 
 @csrf_exempt
@@ -69,24 +68,6 @@ def register(request):
             "status": False,
             "message": "Invalid request method."
         }, status=400)
-    
-@csrf_exempt
-def create_mood_flutter(request):
-    if request.method == 'POST':
-
-        data = json.loads(request.body)
-        new_mood = MoodEntry.objects.create(
-            user=request.user,
-            mood=data["mood"],
-            mood_intensity=int(data["mood_intensity"]),
-            feelings=data["feelings"]
-        )
-
-        new_mood.save()
-
-        return JsonResponse({"status": "success"}, status=200)
-    else:
-        return JsonResponse({"status": "error"}, status=401)
     
 @csrf_exempt
 def logout(request):
