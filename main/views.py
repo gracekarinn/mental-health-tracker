f1b32c8f7e1d324a3acb04cc6a93dd0a041e01ca
gracekarinn <gracekarina12@gmail.com>
2024-11-13 13:35:57 +0700

    feat: add create mood Flutter endpoint for AJAX requests

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse 
 from main.forms import MoodEntryForm
+from django.http import JsonResponse
 from main.models import MoodEntry
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core import serializers
@@ -8,6 +9,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 import datetime
+import json
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
@@ -127,3 +129,22 @@ def add_mood_entry_ajax(request):
     new_mood.save()
 
     return HttpResponse(b"CREATED", status=201)
+
+@csrf_exempt
+def create_mood_flutter(request):
+    if request.method == 'POST':
+
+        data = json.loads(request.body)
+        new_mood = MoodEntry.objects.create(
+            user=request.user,
+            mood=data["mood"],
+            mood_intensity=int(data["mood_intensity"]),
+            feelings=data["feelings"]
+        )
+
+        new_mood.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
+    
