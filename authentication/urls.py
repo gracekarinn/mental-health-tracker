74061d524d9b9cdb3dc206f5af7d2b266c096627
gracekarinn <gracekarina12@gmail.com>
2024-11-13 12:37:03 +0700

    feat: add authentication URLs for login, registration, and mood creation

---
+++ authentication/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from authentication.views import login, register, create_mood_flutter
+
+app_name = 'authentication'
+
+urlpatterns = [
+    path('login/', login, name='login'),
+    path('register/', register, name='register'),
+    path('create-flutter/', create_mood_flutter, name='create_mood_flutter'),
+]
